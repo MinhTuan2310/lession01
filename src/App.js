d40bd96435b8949fb4ea52ca33675e30b012a32c
Tuan Le <lmtuan2310@gmail.com>
2021-11-17 15:18:20 +0700

    upload new code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from "react-router";
+import "./App.css";
+import PostFeature from "./feature/Post";
+import {Redirect} from "react-router-dom";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+          <Redirect exact from="/" to="/posts" />
+          <Route path="/posts" component={PostFeature}  />
+      </Switch>
     </div>
   );
 }
